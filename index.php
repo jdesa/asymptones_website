84779bab495e21bcea09ab9eac86b42e39c320c4
Jacqui De Sa <jdesa@mit.edu>
2015-12-30 20:49:20 -0700

    First commit with dummy text

---
+++ index.php
@@ -0,0 +1,357 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta https-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="sun2.png">
+
+    <title>The MIT Asymptones</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+
+    <!-- Custom Fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css" type="text/css">
+
+    <!-- Plugin CSS -->
+    <link rel="stylesheet" href="css/animate.min.css" type="text/css">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/creative.css" type="text/css">
+    <link rel="stylesheet" href="css/video-player.css" type="text/css">
+</head>
+
+
+<body id="page-top">
+
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">MIT Asymptones</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+
+                    <li>
+                        <a class="page-scroll" href="#about">About</a>
+                    </li>
+                    
+                    <li>
+                        <a class="page-scroll" href="#listen">Listen</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#join">Members</a>
+                    </li>
+
+                    <li>
+                        <a class="page-scroll" href="#about">Audition</a>
+                    </li>
+
+                    <li>
+                        <a class="page-scroll" href="#about">Gigs & Requests</a>
+                    </li>
+
+                    <li>
+                        <a class="page-scroll" href="#about">Contact</a>
+                    </li>
+                
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+    <header>
+        <div class="header-content">
+        </div>
+    </header>
+
+
+    <section class="bg-primary" id="about" >
+        <div class="container" style="opacity:1.0">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 class="section-heading">About</h2>
+                    <hr class="light">
+
+                    <p class="text" style="text-align:justify">We are the Asymptones. We take a low-key approach to a cappella singing! The group was started in 2007 when a few MIT students started singing together for fun, and grew from there into our group today. The Asymptones are dedicated to being a low-time-commitment group that sings for fun! The group was started to let us sing fun songs without dedicating 6+ hours to rehearsal every week. Right now we sing only 3 hours per week! We sing everything from Guitar Hero hits to Disney songs to video game music and are constantly arranging new music in arranging parties.</p>
+                    
+                    <p class="text" style="text-align:justify">We are the Asymptones. We take a low-key approach to a cappella singing! The group was started in 2007 when a few MIT students started singing together for fun, and grew from there into our group today. The Asymptones are dedicated to being a low-time-commitment group that sings for fun! The group was started to let us sing fun songs without dedicating 6+ hours to rehearsal every week. Right now we sing only 3 hours per week! We sing everything from Guitar Hero hits to Disney songs to video game music and are constantly arranging new music in arranging parties.</p>
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+        
+
+    <section id="listen">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading">Listen</h2>
+                    <hr class="primary" style = "margin-bottom:50px">
+                </div>
+            </div>
+        </div>
+        <div class="container">
+
+        <!-- THE YOUTUBE PLAYER -->
+        <div class="vid-container">
+
+            <iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/119202759&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
+        </div>
+
+        <div style = "margin:75px"> </div>
+
+        <!-- THE YOUTUBE PLAYER -->
+        <div class="vid-container" style = "margin-bottom:50px">
+            <iframe id="vid_frame" src="https://youtube.com/embed/5u3K9Gn66Hk?autoplay=0&rel=0&showinfo=0&autohide=1" frameborder="0" width="560" height="315"></iframe>
+        </div>
+
+
+        <!-- THE PLAYLIST -->
+        <div class="vid-list-container">
+            <div class="vid-list">
+
+                <div class="vid-item" onClick="document.getElementById('vid_frame').src='https://youtube.com/embed/5u3K9Gn66Hk?autoplay=1&rel=0&showinfo=0&autohide=1'">
+                  <div class="thumb"><img src="https://img.youtube.com/vi/5u3K9Gn66Hk/0.jpg"></div>
+                  <div class="desc">Friday(Matt Mulholland)</div>
+                </div>
+                
+                <div class="vid-item" onClick="document.getElementById('vid_frame').src='https://youtube.com/embed/UxCYr9dg8PY?autoplay=1&rel=0&showinfo=0&autohide=1'">
+                  <div class="thumb"><img src="https://img.youtube.com/vi/UxCYr9dg8PY/0.jpg"></div>
+                  <div class="desc">Baba Yetu (Christopher Tin)</div>
+                </div>
+
+                <div class="vid-item" onClick="document.getElementById('vid_frame').src='https://youtube.com/embed/LyCjHx57Bik?autoplay=1&rel=0&showinfo=0&autohide=1'">
+                  <div class="thumb"><img src="https://img.youtube.com/vi/LyCjHx57Bik/0.jpg"></div>
+                  <div class="desc">Foil (Weird Al)</div>
+                </div>
+
+                <div class="vid-item" onClick="document.getElementById('vid_frame').src='https://youtube.com/embed/m7OlzZPxit0?autoplay=1&rel=0&showinfo=0&autohide=1'">
+                  <div class="thumb"><img src="https://img.youtube.com/vi/m7OlzZPxit0/0.jpg"></div>
+                  <div class="desc">Nemesis (Johnathan Coulton)</div>
+                </div>
+
+                <div class="vid-item" onClick="document.getElementById('vid_frame').src='https://youtube.com/embed/zlvHbdEjd9Q?autoplay=1&rel=0&showinfo=0&autohide=1'">
+                  <div class="thumb"><img src="https://img.youtube.com/vi/zlvHbdEjd9Q/0.jpg"></div>
+                  <div class="desc">Title of the Song & The Fox (Ylvis)</div>
+                </div>
+
+                <div class="vid-item" onClick="document.getElementById('vid_frame').src='https://youtube.com/embed/ygBXltkXKhU?autoplay=1&rel=0&showinfo=0&autohide=1'">
+                  <div class="thumb"><img src="https://img.youtube.com/vi/ygBXltkXKhU/0.jpg"></div>
+                  <div class="desc">Doctor Who Theme</div>
+                </div>
+
+                <div class="vid-item" onClick="document.getElementById('vid_frame').src='https://youtube.com/embed/fn12zl9Q8LU?autoplay=1&rel=0&showinfo=0&autohide=1'">
+                  <div class="thumb"><img src="https://img.youtube.com/vi/fn12zl9Q8LU/0.jpg"></div>
+                  <div class="desc">SCV Love Song (Nerd Song)</div>
+                </div>
+
+
+        </div>
+
+        <!-- LEFT AND RIGHT ARROWS -->
+        <div class="arrows">
+            <div class="arrow-left" style="background-color:#69154e"><i class="fa fa-chevron-left fa-lg" ></i></div>
+            <div class="arrow-right" style="background-color:#69154e"><i class="fa fa-chevron-right fa-lg" ></i></div>
+        </div>
+
+        </div>
+
+       
+        </div>
+
+    </section>
+
+    <section class="no-padding" id="portfolio">
+        <div class="container-fluid">
+            <div class="row no-gutter">
+                <div class="col-lg-4 col-sm-6">
+                    <a href="#" class="portfolio-box">
+                        <img src="img/ziv1.png" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Ziv
+                                </div>
+                                <div class="project-name">
+                                    It's a Ziv!
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="#" class="portfolio-box">
+                        <img src="img/horkley1.png" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Horkles
+                                </div>
+                                <div class="project-name">
+                                    woo
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="#" class="portfolio-box">
+                        <img src="img/jacqui1.png" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Guess
+                                </div>
+                                <div class="project-name">
+                                    sadashdilasdila
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="#" class="portfolio-box">
+                        <img src="img/cecilia1.png" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Cecilia
+                                </div>
+                                <div class="project-name">
+                                    sasafhaslif
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="#" class="portfolio-box">
+                        <img src="img/tessa1.png" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Tessa
+                                </div>
+                                <div class="project-name">
+                                    asbf,sabfash
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="#" class="portfolio-box">
+                        <img src="img/arin1.png" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Arin
+                                </div>
+                                <div class="project-name">
+                                    ahsildsahlidas
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="join" class="bg-primary" style="padding-bottom:50px;padding-top:50px;">
+        <div class="container" style = "margin-bottom:50px;margin-top:50px">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 class="section-heading">Audition</h2>
+                    <hr class="light">
+
+                    <p class="text" style="text-align:justify">
+                    Interested in auditioning? Fall auditions are held the weekend before term starts (come see us at the Fall Midway for details!) and we sometimes holds IAP or spring auditions depending on the year. Each audition is around 7 minutes long and involves some vocal warmups and excercises followed by a solo of your choice (it doesn't have to be nerdy)! Email us at asymptones-officers@mit.edu to schedule an audition or if you have any questions!
+                    </p>
+        </div>
+    </section>
+
+    <section id="join" class="bg-light" style="padding-bottom:50px;padding-top:50px;">
+        <div class="container" style = "margin-bottom:50px;margin-top:50px">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 class="section-heading">Gigs and Song Requests</h2>
+                    <hr>
+
+                    <p class="text" style="text-align:justify">
+                    Interested in auditioning? Fall auditions are held the weekend before term starts (come see us at the Fall Midway for details!) and we sometimes holds IAP or spring auditions depending on the year. Each audition is around 7 minutes long and involves some vocal warmups and excercises followed by a solo of your choice (it doesn't have to be nerdy)! Email us at asymptones-officers@mit.edu to schedule an audition or if you have any questions!
+                    </p>
+        </div>
+    </section>
+
+<!-- Footer -->
+    <footer class="text-center">
+        <div class="footer-below">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12" style="font-size:16px">
+                        <center>
+                        Got any questions? Email us at asymptones-officers@mit.edu. Thanks!
+                    </center>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+        <script type="text/javascript">
+        $(document).ready(function () {
+            $(".arrow-right").bind("click", function (event) {
+                event.preventDefault();
+                $(".vid-list-container").stop().animate({
+                    scrollLeft: "+=336"
+                }, 750);
+            });
+            $(".arrow-left").bind("click", function (event) {
+                event.preventDefault();
+                $(".vid-list-container").stop().animate({
+                    scrollLeft: "-=336"
+                }, 750);
+            });
+        });
+    </script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="js/jquery.easing.min.js"></script>
+    <script src="js/jquery.fittext.js"></script>
+    <script src="js/wow.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="js/creative.js"></script>
+
+</body>
+
+</html>
